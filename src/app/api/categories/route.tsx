c77ce886c18c0db9baecf862558c8af7e9299d8b
Carlos Osorio <gimcode6@gmail.com>
2025-04-07 19:21:10 -0600

    Update Route.tsx API

--- src/app/api/categories/route.tsx
+++ src/app/api/categories/route.tsx
@@ -2,6 +2,7 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import { NextResponse } from 'next/server';
 
+
 export async function GET() {
     try {
         // Comprobamos la ruta al archivo
@@ -20,12 +21,22 @@ export async function GET() {
             words
         }));
 
-        return NextResponse.json(transformed); // Respuesta exitosa
-    } catch (error : any) {
-        // Agregar más detalles en el error para depurar
-        console.error('Error al leer el archivo:', error);
-
-        // Usar NextResponse para manejar el error
-        return NextResponse.json({ error: 'Error al leer categories.json', details: error.message }, { status: 500 });
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Error al leer el archivo:', error);
+    
+            return NextResponse.json(
+                { error: 'Error al leer categories.json', details: error.message },
+                { status: 500 }
+            );
+        } else {
+            // Por si el universo decide lanzar otra cosa, como un string o un gremlin
+            console.error('Error desconocido al leer el archivo:', error);
+    
+            return NextResponse.json(
+                { error: 'Error desconocido al leer categories.json' },
+                { status: 500 }
+            );
+        }
     }
 }
