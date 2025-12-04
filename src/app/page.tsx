f137d5f705d643fa0c3684f9b23633d8ffb24a9a
Carlos Osorio <gimcode6@gmail.com>
2025-12-03 22:44:38 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
     <div className={getThemeClasses(isDarkMode)}>
       <header className={`py-6 px-8 shadow-md ${isDarkMode ? 'bg-[#1e293b]' : 'bg-white'} flex justify-between items-center`}>
         <h1 className="text-5xl font-extrabold bg-gradient-to-br from-yellow-300 to-yellow-500 text-transparent bg-clip-text drop-shadow-lg tracking-wide italic">
-          🐝 Spelling Bee 2025
+          🐝 BeeGreat 2025
         </h1>
         <button 
           onClick={toggleTheme}
